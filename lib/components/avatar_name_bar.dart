80889745b29ec40198c545a91b876a7441e00adb
hadson0 <hadsonsb2221@gmail.com>
2021-09-10 00:29:28 -0300

    Created avatar_name_bar and avatar_tag_bar

---
+++ lib/components/avatar_name_bar.dart
@@ -0,0 +1,44 @@
+import 'package:flutter/material.dart';
+
+import 'package:drawme/models/avatar.dart';
+
+class AvatarNameBar extends StatelessWidget {
+  const AvatarNameBar(
+    this.avatar, {
+    Key? key,
+  }) : super(key: key);
+
+  final Avatar avatar;
+
+  @override
+  Widget build(BuildContext context) {
+    return Container(
+      height: 50,
+      width: double.infinity,
+      decoration: BoxDecoration(
+        color: Colors.grey.shade900,
+      ),
+      child: Column(
+        mainAxisAlignment: MainAxisAlignment.center,
+        children: [
+          Text(
+            avatar.name,
+            style: TextStyle(
+              fontFamily: 'RobotoCondensed',
+              fontSize: 20,
+              color: Colors.white,
+            ),
+          ),
+          Text(
+            avatar.author,
+            style: TextStyle(
+              fontFamily: 'RobotoCondensed',
+              color: Colors.blue,
+              fontSize: 15,
+            ),
+          ),
+        ],
+      ),
+    );
+  }
+}
---
+++ lib/components/avatar_tag_bar.dart
@@ -0,0 +1,56 @@
+import 'package:flutter/material.dart';
+
+import 'package:drawme/models/avatar.dart';
+
+class AvatarTagBar extends StatelessWidget {
+  final Avatar avatar;
+  const AvatarTagBar(
+    this.avatar,{
+    Key? key,
+  }) : super(key: key);
+
+
+  @override
+  Widget build(BuildContext context) {
+    return Container(
+      height: 35,
+      width: double.infinity,
+      padding: const EdgeInsets.symmetric(horizontal: 10),
+      decoration: BoxDecoration(
+        color: Colors.grey.shade400,
+      ),
+      child: Row(
+        children: [
+          Icon(
+            Icons.tag_rounded,
+            color: Colors.blue.shade900,
+          ),
+          SizedBox(width: 6),
+          SingleChildScrollView(
+            child: Row(
+              children: avatar.tags.map((avt) {
+                return Padding(
+                  padding: const EdgeInsets.symmetric(
+                    horizontal: 3,
+                  ),
+                  child: Container(
+                    padding: const EdgeInsets.all(5),
+                    decoration: BoxDecoration(
+                      color: Colors.grey.shade100,
+                      border: Border.all(color: Colors.black),
+                      borderRadius: BorderRadius.circular(20),
+                    ),
+                    child: Text(
+                      avt,
+                      style: TextStyle(color: Colors.blue.shade900),
+                    ),
+                  ),
+                );
+              }).toList(),
+            ),
+          ),
+        ],
+      ),
+    );
+  }
+}
